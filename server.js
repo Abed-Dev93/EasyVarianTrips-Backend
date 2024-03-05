253b2752e13cdebcf6eb05f0820b613aea471e92
Abdulrahman Ghassa <abdulrahman.ghassa93@gmail.com>
2024-03-05 09:09:20 +0200

    Add Vercel Link

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const app = express()
 const port = process.env.PORT
 
 app.use(cors({
-    origin: 'http://localhost:3000',
+    origin: 'https://easy-varian-trips-frontend-ax0xm42kt.vercel.app',
     credentials: true,
     optionsSuccessStatus: 200
 }))
